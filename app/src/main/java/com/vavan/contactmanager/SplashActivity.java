624526da75ab09904047c0e4f0e6462f9f7b8c8d
amperas17 <amperas17@gmail.com>
2015-11-13 18:04:47 +0300

    SplashActivity done using Handler.Callback interface

---
+++ app/src/main/java/com/vavan/contactmanager/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.vavan.contactmanager;
+
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class SplashActivity extends AppCompatActivity implements Handler.Callback {
+
+    private static final int MSG_CONTINUE = 101;
+    private static final long DELAY = 3000;
+    private Handler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mHandler = new Handler(getMainLooper(), this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_CONTINUE), DELAY);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mHandler.removeCallbacksAndMessages(null);
+
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        if (msg.what == MSG_CONTINUE) {
+            startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            finish();
+        }
+        return true;
+    }
+}
